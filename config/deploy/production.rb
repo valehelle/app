8f316fc9070af490f32717d8483050871838b2e2
valehelle <hazmiirfan92@gmail.com>
2017-03-13 21:26:10 +0800

    change server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '172.104.43.57', user: 'root', roles: %w{app db web}
+server '172.104.58.36', user: 'root', roles: %w{app db web}
 
 # role-based syntax
 # ==================
