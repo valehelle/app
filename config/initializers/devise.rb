10cba7c82aa337f87e3bfdf227832220e0fe3a03
valehelle <hazmiirfan92@gmail.com>
2017-03-14 21:17:38 +0800

    change mailer default url option

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-  ActionMailer::Base.default_url_options = { :host => "localhost:3000" }
+  ActionMailer::Base.default_url_options = { :host => "easyorder.club" }
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
