4020f6974eab649e56e57162bf61776867905f8d
valehelle <hazmiirfan92@gmail.com>
2017-03-22 23:48:22 +0800

    Edit UI and UX

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class DashboardController < ApplicationController
     before_action :authenticate_user!
     def show
-        
+        @new_orders = current_user.receipt.where(status: "Buyer Paid").count
+        @total_forms = current_user.form.count
+        @total_products = current_user.product.count
+        @total_orders = current_user.receipt.count
     end
 end
