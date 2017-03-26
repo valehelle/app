1e551a5468e4d52fb3bf52e300886533b1c12220
valehelle <hazmiirfan92@gmail.com>
2017-03-26 14:59:48 +0800

    Good for alpha

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -13,6 +13,20 @@ class ProductController < ApplicationController
     def new
     end
 
+    def edit
+        @product = current_user.product.find(params[:id])
+    end
+
+    def update
+        @product = Product.find(params[:id])
+        if @product.user_id == current_user.id
+            @product.update_attributes(product_params)
+            redirect_to product_index_path()
+        else
+            puts "NOT SAME"
+        end
+    end
+
     def create
         @product = current_user.product.build(product_params)
         @product.save!
@@ -22,6 +36,6 @@ class ProductController < ApplicationController
 
     private 
         def product_params
-            params.require(:product).permit(:name,:form_id)
+            params.require(:product).permit(:name,:form_id,:stock,:desc,:price,:shipping_price)
         end
 end
