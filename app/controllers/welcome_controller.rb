bcc6da874d626fa151e7833d18fe7b222c42279e
valehelle <hazmiirfan92@gmail.com>
2017-03-26 22:26:45 +0800

    Update UI

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   layout "empty"
   def index
     if user_signed_in?
-      redirect_to form_index_path
+      redirect_to dashboard_path
     end
   end
 end
